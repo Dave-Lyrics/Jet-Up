714bbe6500f96c1ab88cb24387fc51d124ef7995
Dave-Lyrics <davidakintoye2008@gmail.com>
2025-11-06 10:35:52 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Wait until the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function (){
+
+    // Get DOM elements
+    const hamburger = document.querySelector('.hamburgger');
+    const navMenu = document.querySelector('.menu');
+
+    //Toggle menu when hamburger is clicked
+    hamburger.addEventListener('click', function (){
+        navMenu.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+
+    // Handle wimdow resize: auto-close mobile menu on desktop view
+    window.addEventListener('resize', function(){
+        if (window.innerWidth > 768) {
+            // On desktop, ensure menu is visible and hambugger is reset
+            navMenu.classList.remove('active')
+            hamburger.classList.remove('active')
+        };
+    });
+});
